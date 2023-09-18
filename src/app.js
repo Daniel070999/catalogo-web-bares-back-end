66cea72d7dc63a55c0fdc204a18fc3b995d00eaa
Daniel070999 <danielixo99@gmail.com>
2023-09-17 19:17:24 -0500

    se agrega img para menus

--- src/app.js
+++ src/app.js
@@ -11,9 +11,10 @@ const cors = require('cors');
 const cookieParser = require("cookie-parser");
 const bodyParser = require('body-parser');
 
-const app = express();
+const app =  express();
 
 app.use(express.json());
+
 app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: false }));
--- src/routes/files/files.js
+++ src/routes/files/files.js
@@ -3,11 +3,25 @@ const router = express.Router();
 const path = require('path');
 const fs = require('fs');
 
-const imagesDirectory = path.join(__dirname, '..', '..', '..', 'uploads', 'images', 'logo');
+const directoryLogo = path.join(__dirname, '..', '..', '..', 'uploads', 'images', 'logo');
+const directoryImageMenu = path.join(__dirname, '..', '..', '..', 'uploads', 'images', 'menu');
 
-router.get('/images/:imageName', (req, res) => {
+router.get('/logobar/:imageName', (req, res) => {
     const imageName = req.params.imageName;
-    const imagePath = path.join(imagesDirectory, imageName);
+    const imagePath = path.join(directoryLogo, imageName);
+    // Verifica si el archivo de imagen existe en el directorio
+    if (fs.existsSync(imagePath)) {
+        // Si existe, envía el archivo de imagen como respuesta
+        res.sendFile(imagePath);
+    } else {
+        // Si no existe, envía una respuesta de error
+        res.status(404).send('Imagen no encontrada');
+    }
+});
+
+router.get('/logomenu/:imageName', (req, res) => {
+    const imageName = req.params.imageName;
+    const imagePath = path.join(directoryImageMenu, imageName);
     // Verifica si el archivo de imagen existe en el directorio
     if (fs.existsSync(imagePath)) {
         // Si existe, envía el archivo de imagen como respuesta
